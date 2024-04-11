b4c184069dc392781b16973b542d7d0fa735d06b
Sumin Kim <411sumin@gmail.com>
2024-04-11 17:12:10 +0900

    2024.04.11
    extra edits to the slidable function

--- must_eat_place_app_firebase/lib/view/home.dart
+++ must_eat_place_app_firebase/lib/view/home.dart
@@ -55,82 +55,89 @@ class _HomeState extends State<Home> {
   // ---FUNCTIONS---
   Widget _buildItemWidget(QueryDocumentSnapshot<Object?> doc) {
     final restaurant = Restaurant(
-      name: doc['name'] ,
-      phone: doc['phone'] ,
-      estimate: doc['estimate'] ,
-      lat: doc['lat'] ,
-      lng: doc['lng'] ,
-      image: doc['image'] ,
+      name: doc['name'],
+      phone: doc['phone'],
+      estimate: doc['estimate'],
+      lat: doc['lat'],
+      lng: doc['lng'],
+      image: doc['image'],
     );
-    return GestureDetector(
-      onTap: () {
-        Get.to(const Update(), arguments: [
-          doc.id, // Document ID
-          doc['name'] ,
-          doc['phone'] ,
-          doc['estimate'] ,
-          doc['lat'] ,
-          doc['lng'],
-          doc['image'] ,
-        ]);
-      },
-      child: Slidable(
-        startActionPane:ActionPane(
-          motion: const DrawerMotion(), 
-          children: [
-            SlidableAction(
-              backgroundColor: Colors.orange,
-              icon:  Icons.map_rounded,
-              label: 'Map',
-              onPressed: (context) {
-                Get.to(
-                  ()=> const Location(),
-                  arguments: [
-                    double.parse(doc['lat']),
-                    double.parse(doc['lng']),
-                  ]
-                );
-              },
-            ),
-          ]
-          ),
-        child: Slidable(
-          endActionPane: ActionPane(
-            motion: const DrawerMotion(), 
+    
+    // Remove nested Slidable widgets
+    return Slidable(
+        // Start action pane for the map action
+        startActionPane: ActionPane(
+            motion: const DrawerMotion(),
             children: [
-              SlidableAction(
-                backgroundColor: Colors.red,
-                icon: Icons.delete_forever,
-                label: 'Delete',
-                onPressed: (context) async {
-                  await FirebaseFirestore.instance
-                  .collection('musteatplace')
-                  .doc(doc.id)
-                  .delete();
-                },
-              )
-            ]
-          ),
-          child: Card(
-            child: ListTile(
-              title: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      restaurant.image,
-                      width: 80,
-                    ),
-                  ),
-                  Text(
-                    "NAME : ${restaurant.name}\n\nPHONE : ${restaurant.phone}",
-                  ),
-                ],
-              ),
-            ),
-          ),
+                SlidableAction(
+                    backgroundColor: Colors.orange,
+                    icon: Icons.map_rounded,
+                    label: 'Map',
+                    onPressed: (context) {
+                        Get.to(
+                            () => const Location(),
+                            arguments: [
+                                double.parse(doc['lat']),
+                                double.parse(doc['lng']),
+                            ],
+                        );
+                    },
+                ),
+            ],
         ),
-      ),
+        
+        // End action pane for the delete action
+        endActionPane: ActionPane(
+            motion: const DrawerMotion(),
+            children: [
+                SlidableAction(
+                    backgroundColor: Colors.red,
+                    icon: Icons.delete_forever,
+                    label: 'Delete',
+                    onPressed: (context) async {
+                        await FirebaseFirestore.instance
+                        .collection('musteatplace')
+                        .doc(doc.id)
+                        .delete();
+                    },
+                ),
+            ],
+        ),
+        
+        // Child widget
+        child: GestureDetector(
+            onTap: () {
+                Get.to(const Update(), arguments: [
+                    doc.id, // Document ID
+                    doc['name'],
+                    doc['phone'],
+                    doc['estimate'],
+                    doc['lat'],
+                    doc['lng'],
+                    doc['image'],
+                ]);
+            },
+            
+            // ListTile inside Card
+            child: Card(
+                child: ListTile(
+                    title: Row(
+                        children: [
+                            Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                    restaurant.image,
+                                    width: 80,
+                                ),
+                            ),
+                            Text(
+                                "NAME : ${restaurant.name}\n\nPHONE : ${restaurant.phone}",
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        ),
     );
-  }
+}
 }
